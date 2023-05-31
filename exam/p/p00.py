9a528a5b1baebf2ee8d05523643cfe99721969f5
Ashley Chu <125455673+chihlingchu@users.noreply.github.com>
2023-05-31 18:01:52 +0800

    Update p00.py

--- exam/p/p00.py
+++ exam/p/p00.py
@@ -1,6 +1,10 @@
 def p00(n=0):
     even=None
     # ↓程式區域↓
-
+    num = int(input("number: "))
+    if (num % 2) == 0:
+      print("True".format(num))
+    else:
+      print("False".format(num))
     # ↑程式區域↑
     return even
